5450fe1112f7a27235eab1fbdc3afa6e2d2ed3b9
Saeyoung Kim <heehouse1@gmail.com>
2018-02-03 15:14:37 +0900

    first commit

---
+++ src/main/java/com/skcc/teamcrystal/TeamcrystalApplication.java
@@ -0,0 +1,12 @@
+package com.skcc.teamcrystal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcrystalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcrystalApplication.class, args);
+	}
+}
