b6bf0c2b53c775f0df16f9a8f286c20ee507eed3
heeho <heehouse1@gmail.com>
2018-02-06 09:08:16 +0900

    Log In

--- src/main/java/com/skcc/teamcrystal/backend/TeamcrystalApplication.java
+++ src/main/java/com/skcc/teamcrystal/backend/TeamcrystalApplication.java
@@ -1,4 +1,4 @@
-package com.skcc.teamcrystal;
+package com.skcc.teamcrystal.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
