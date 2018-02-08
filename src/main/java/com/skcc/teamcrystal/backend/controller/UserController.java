f06379e445bec336851d8ea12ad9e23dff25444b
heeho <heehouse1@gmail.com>
2018-02-08 19:14:21 +0900

    Remove Useless Library

--- src/main/java/com/skcc/teamcrystal/backend/controller/UserController.java
+++ src/main/java/com/skcc/teamcrystal/backend/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.skcc.teamcrystal.backend.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
